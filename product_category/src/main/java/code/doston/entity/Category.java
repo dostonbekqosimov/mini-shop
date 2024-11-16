8f1c011a1118395af14d671a86dcefcee0818ed2
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-16 15:21:06 +0500

    Add Category and Product entitis

---
+++ product_category/src/main/java/code/doston/entity/Category.java
@@ -0,0 +1,28 @@
+package code.doston.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer orderNumber;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+}
---
+++ product_category/src/main/java/code/doston/entity/Product.java
@@ -0,0 +1,33 @@
+package code.doston.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+    private LocalDateTime createdDate;
+}
